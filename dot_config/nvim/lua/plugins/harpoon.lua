0bdd2d260cac9228cd1af622a2068dc3605b165f
Ryan Malonzo <64932650+yusa-ai@users.noreply.github.com>
2024-03-08 17:02:04 +0100

    Update

---
+++ dot_config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,55 @@
+return {
+  "ThePrimeagen/harpoon",
+  keys = {
+    {
+      "<leader>h",
+      function()
+        require("harpoon"):list():append()
+      end,
+      desc = "Harpoon file",
+    },
+    {
+      "<leader>H",
+      function()
+        local harpoon = require("harpoon")
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end,
+      desc = "Harpoon quick menu",
+    },
+    {
+      "<leader>&",
+      function()
+        require("harpoon"):list():select(1)
+      end,
+      desc = "Harpoon to file 1",
+    },
+    {
+      "<leader>é",
+      function()
+        require("harpoon"):list():select(2)
+      end,
+      desc = "Harpoon to file 2",
+    },
+    {
+      '<leader>"',
+      function()
+        require("harpoon"):list():select(3)
+      end,
+      desc = "Harpoon to file 3",
+    },
+    {
+      "<leader>'",
+      function()
+        require("harpoon"):list():select(4)
+      end,
+      desc = "Harpoon to file 4",
+    },
+    {
+      "<leader>(",
+      function()
+        require("harpoon"):list():select(5)
+      end,
+      desc = "Harpoon to file 5",
+    },
+  },
+}
