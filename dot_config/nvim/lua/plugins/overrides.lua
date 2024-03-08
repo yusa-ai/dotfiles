e537341c3843619feb53765b9337346fffe56258
Ryan Malonzo <64932650+yusa-ai@users.noreply.github.com>
2024-03-08 11:58:28 +0100

    Update

---
+++ dot_config/nvim/lua/plugins/overrides.lua
@@ -0,0 +1,27 @@
+return {
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = { eslint = {} },
+      setup = {
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
+}
